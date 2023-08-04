1249f51a94bd6d6be620264d540eaf3064b1dfbd
reddsoligarch <55617587+deweezy12@users.noreply.github.com>
2023-08-04 15:22:42 +0200

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+"""Degines URL patterns for users"""
+
+from django.urls import path, include
+
+app_name = 'users'
+urlpatterns = [
+	#Include default auth urls.
+	path('', include('django.contrib.auth.urls')),
+]
